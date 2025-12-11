d0caa4571461d6d4bc87e7068f62c5879cce4d90
davidade300 <iaderinho@icloud.com>
2025-12-11 20:33:45 -0300

    rotas aninhadas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
-import {Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 export default function App() {
     return (
-        <nav style={{borderBottom: "solid 1px", paddingBottom: "1rem"}}>
-            <Link to="/invoices">Invoices</Link> |{" "}
-            <Link to="/expenses">Expenses</Link>
-        </nav>
+        <>
+            <nav style={{borderBottom: "solid 1px", paddingBottom: "1rem"}}>
+                <Link to="/invoices">Invoices</Link> |{" "}
+                <Link to="/expenses">Expenses</Link>
+            </nav>
+            <Outlet/>
+        </>
     );
 }
