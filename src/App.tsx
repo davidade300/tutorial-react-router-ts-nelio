c15fce01223ebe6623004a3270c6bf94a6f07c65
davidade300 <iaderinho@icloud.com>
2025-12-11 20:23:14 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default function App() {
-  return (
-    <nav
-      style={{
-        borderBottom: "solid 1px",
-        paddingBottom: "1rem",
-      }}
-    >
-      <Link to="/invoices">Invoices</Link> |{" "}
-      <Link to="/expenses">Expenses</Link>
-    </nav>
-  );
+    return (
+        <nav style={{borderBottom: "solid 1px", paddingBottom: "1rem"}}>
+            <Link to="/invoices">Invoices</Link> |{" "}
+            <Link to="/expenses">Expenses</Link>
+        </nav>
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Invoices from "./routes/Invoices/invoices.tsx";
+import Expenses from "./routes/Expenses";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>,
-  </StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="expenses" element={<Expenses/>}/>
+                <Route path="invoices" element={<Invoices/>}/>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
 );
